57f462034e26680ebe9567f4d3e00b5565d34b88
hsanchez <huascar.sanchez@gmail.com>
2016-12-15 20:56:23 -0800

    bunch of changes.. new workflow: first generate the stop list from all projects. this list will contain the intersection of words that occur a lot (more than 10 or 20 times in each project).

--- src/main/java/com/vesperin/partition/cmds/ProcessProjects.java
+++ src/main/java/com/vesperin/partition/cmds/ProcessProjects.java
@@ -10,24 +10,30 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.vesperin.partition.BasicCli;
 import com.vesperin.partition.spi.Git;
-import com.vesperin.partition.utils.GroupMaker;
 import com.vesperin.partition.utils.Projects;
 import com.vesperin.text.Grouping;
 import com.vesperin.text.Project;
 import com.vesperin.text.Selection;
 import com.vesperin.text.spi.BasicExecutionMonitor;
 import com.vesperin.text.spi.ExecutionMonitor;
+import com.vesperin.text.utils.Samples;
 
 import javax.inject.Inject;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static com.vesperin.text.Selection.Word;
@@ -40,7 +46,7 @@ import static java.nio.file.StandardOpenOption.CREATE;
 @SuppressWarnings("FieldCanBeLocal") @Command(name = "p", description = "Process a list of projects")
 public class ProcessProjects implements BasicCli.CliCommand {
 
-  final ExecutionMonitor MONITOR = BasicExecutionMonitor.get();
+  private final ExecutionMonitor MONITOR = BasicExecutionMonitor.get();
 
   @Inject HelpOption<ProcessProjects> help;
 
@@ -66,6 +72,9 @@ public class ProcessProjects implements BasicCli.CliCommand {
 
   @Option(name = {"-k", "--topk"}, arity = 1, description = "Select top k words from each project. Default is 75.")
   private int topk = 75;
+
+  @Option(name = {"-i", "--inc"}, description = "Partitions projects in small increments: [[[1], 2], 3]")
+  private boolean inc = false;
 
 
   @Option(name = {"-v", "--verbose"}, description = "Prints logging messages")
@@ -119,46 +128,52 @@ public class ProcessProjects implements BasicCli.CliCommand {
 
         final List<Project> projects = Projects.buildProjects(topk, scope, outDir, projectNames);
 
-        final List<List<Project>> pGroups = Lists.newArrayList();
-
         final Map<String, Project> index = new HashMap<>();
         projects.forEach(p -> index.put(p.name(), p));
 
-        final Grouping.Groups groups = groupBy(grouping, overlap, projects);
-        for(Grouping.Group each : groups){
-          final List<Project> pList = Lists.newArrayList();
-          for(Object o : each){
-            final Project p = (Project) o;
-            pList.add(p);
+        if(inc){
+
+          MONITOR.info("Ignoring " + out + " since we are clustering projects in small steps; e.g., step1.json");
+
+          final Collection<Callable<Void>> tasks = Lists.newArrayList();
+
+          final int n = projects.size() + 1;
+          for(int step = 1; step <= n; step++){
+            final List<Project> sublist = projects.stream()
+              .limit(step)
+              .collect(Collectors.toList());
+
+            final String name = Objects.isNull(out) ? null : "step" + step + ".json";
+
+            tasks.add(() -> {
+              try {
+                formClusters(name, sublist);
+              } catch (IOException e) {
+                MONITOR.error("Unable to form cluster", e);
+              }
+
+              return null;
+            });
+
           }
 
-          pGroups.add(pList);
-        }
+          final ExecutorService service = scaleExecutor(projects.size());
 
+          try {
 
-        final Clusters  clusters = new Clusters(pGroups);
-        final Gson      gson     = new GsonBuilder()
-          .setPrettyPrinting()
-          .create();
+            service.invokeAll(tasks);
 
-        if(Objects.isNull(out)){
+          } catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+          }
 
-          MONITOR.info(gson.toJson(clusters));
+          shutdownService(service);
 
         } else {
-
-          final Path newFile = Paths.get(out);
-          Files.deleteIfExists(newFile);
-
-          Files.write(
-            newFile,
-            gson.toJson(clusters).getBytes(),
-            CREATE,
-            APPEND
-          );
-
-          MONITOR.info(String.format("%s was created.", out));
+          formClusters(out, projects);
         }
+
+
       } catch (Exception e){
         e.printStackTrace(System.err);
         return -1;
@@ -167,6 +182,77 @@ public class ProcessProjects implements BasicCli.CliCommand {
     }
 
     return 0;
+  }
+
+  private static ExecutorService scaleExecutor(int scale){
+    final int cpus       = Runtime.getRuntime().availableProcessors();
+    scale                = scale > 10 ? 10 : scale;
+    final int maxThreads = ((cpus * scale) > 0 ? (cpus * scale) : 1);
+
+    return Executors.newFixedThreadPool(maxThreads);
+  }
+
+  private static void shutdownService(ExecutorService service){
+    shutdownService(500, service);
+  }
+
+  private static void shutdownService(long timeout, ExecutorService service){
+    // wait for all of the executor threads to finish
+    service.shutdown();
+
+    try {
+      if (!service.awaitTermination(timeout, TimeUnit.SECONDS)) {
+        // pool didn't terminate after the first try
+        service.shutdownNow();
+      }
+
+      if (!service.awaitTermination(timeout * 2, TimeUnit.SECONDS)) {
+        // pool didn't terminate after the second try
+        System.out.println("ERROR: executor service did not terminate after a second try.");
+      }
+    } catch (InterruptedException ex) {
+      service.shutdownNow();
+      Thread.currentThread().interrupt();
+    }
+  }
+
+  private void formClusters(String out, List<Project> projects) throws IOException {
+    final List<List<Project>> pGroups = Lists.newArrayList();
+    final Grouping.Groups groups = groupBy(grouping, overlap, projects);
+
+    for(Grouping.Group each : groups){
+      final List<Project> pList = Lists.newArrayList();
+      for(Object o : each){
+        final Project p = (Project) o;
+        pList.add(p);
+      }
+
+      pGroups.add(pList);
+    }
+
+
+    final Clusters clusters = new Clusters(pGroups);
+    final Gson gson     = new GsonBuilder()
+      .setPrettyPrinting()
+      .create();
+
+    if(Objects.isNull(out)){
+      MONITOR.info(gson.toJson(clusters));
+
+    } else {
+
+      final Path newFile = Paths.get(out);
+      Files.deleteIfExists(newFile);
+
+      Files.write(
+        newFile,
+        gson.toJson(clusters).getBytes(),
+        CREATE,
+        APPEND
+      );
+
+      MONITOR.info(String.format("%s was created.", out));
+    }
   }
 
   private static Grouping.Groups groupBy(int grouping, int overlap, List<Project> projects){
@@ -187,9 +273,12 @@ public class ProcessProjects implements BasicCli.CliCommand {
       for(List<Project> each : groups){
         final Set<String> words = Sets.newHashSet();
 
-        final List<Set<Word>> sorted = each.stream().sorted((a, b) -> Ints.compare(a.wordSet().size(), b.wordSet().size())).map(Project::wordSet).collect(Collectors.toList());
+        final List<Set<Word>> sorted = each.stream()
+          .sorted((a, b) -> Ints.compare(a.wordSet().size(), b.wordSet().size()))
+          .map(Project::wordSet)
+          .collect(Collectors.toList());
 
-        final Set<Selection.Word> ws = GroupMaker.getCommonElements(sorted);
+        final Set<Selection.Word> ws = Samples.getCommonElements(sorted);
         if(ws.size() > 15){
           System.out.print("");
         }
